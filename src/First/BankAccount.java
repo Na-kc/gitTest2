6fb66cf8756f9e822aa6d5659d0ecfd1202bba96
Na-kc <choul0920@gmail.com>
2020-06-15 10:54:28 +0900

    update todo

--- src/First/BankAccount.java
+++ src/First/BankAccount.java
@@ -1,22 +1,43 @@
 package First;
 import java.util.*;
+import java.text.*;
 public class BankAccount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner input = new Scanner(System.in);
-		System.out.println("계좌 123-456789 (예금주 : 홍길동");
+				
 		int account = 10000;
+		int money = 0;
+		String price = "";
+		NumberFormat nf = NumberFormat.getInstance();
+		
+		System.out.println("계좌 123-456789 (예금주 : 홍길동");
+		System.out.printf("잔액 : %d원\n", account);
+		
 		while(true) {
-			System.out.printf("잔액 : %d원\n", account);
+			
 			String bankwork = input.nextLine();
-			int money = 0;
-			if(money >= 0) { account += money; }
-			else { account -= money; }
-			if(account <= 0) {
-				System.out.println("잔액 : 0원 입니다");
-				break;
-			}
+			String intStr = bankwork.replaceAll("[^\\d]", "");
+			money = Integer.parseInt(intStr);
+			
+			if(bankwork.contains("입금")) {
+				if(money >= 0) { account += money; }
+				price = nf.format(account);
+				System.out.println("잔액 : "+ price + "원");
+	        }
+			else if(bankwork.contains("출금")) {
+				if(money >= 0) { account -= money; }
+				price = nf.format(account);
+				if(account <= 0) {
+					System.out.println("[에러] 잔액이 부족합니다.");
+					break;
+				}
+				else {System.out.println("잔액 : "+ price + "원"); }
+	        }
+			else {System.out.println("다시 입력해주십시오.");}
+			
 		}
 		
 	}
